e4338f2b3bfcbd850f2f08c770f79bd4662bc6ae
ZahidulHoque09 <zahin_m96@yahoo.com>
2024-01-28 08:06:50 +0100

    Java script 1

---
+++ Js/main.js
@@ -0,0 +1,128 @@
+alert("Hello world");
+
+console.dir("Hello world:");
+
+console.log("Hello world:");
+
+document.write("Your id has been hacked !");
+
+/* let a = 30;
+let b = 40;
+
+let sum = a + b;
+
+console.log("The sum is :", sum);
+ */
+/* let a = "40";
+let b = 40;
+
+let sum = a + b;
+
+console.log("The sum is:", sum);
+ */
+/* 
+let a = 30;
+let b = 6;
+
+let sum = a * b;
+
+console.log("The multiplication is :", sum);
+ */
+
+/* let a = 60;
+let b = 20;
+
+let sum = a / b;
+
+console.log("The division is :", sum);
+ */
+
+var nam = " Zahidul hoque Molla";
+var nam = "Ayra Mehbin";
+nam = "Nusaibah Mehbin";
+
+console.log(nam);
+
+let x = "Maria Mehbin";
+
+console.log(x);
+
+let y = "Edboskolan";
+
+console.log(y);
+console.log(typeof y);
+
+let z = "Ostra grundskolan ";
+
+console.log(z);
+
+const pi = 3.171;
+console.log(typeof pi);
+
+console.log(pi);
+
+let bol = true;
+console.log(bol);
+console.log(typeof bol);
+
+let m;
+console.log(m);
+
+let n = null;
+console.log(n);
+
+let s = 10;
+console.log("The result is:", s ** 4);
+
+let p = 10;
+let incre = ++p;
+console.log("The increment is:", incre);
+
+let q = 10;
+let decrement = --q;
+console.log("The Decrement is:", decrement);
+
+let a = 5;
+a = a + 10;
+a = a + 20;
+console.log("The result is :", a);
+
+let e = 5;
+console.log("The first e:", e);
+e = e + 10;
+console.log("The second e is:", e);
+e = e + 20;
+console.log("The third e is:", e);
+
+let f = 30;
+f = f - 10;
+f -= 30;
+console.log("The result of subtraction:", f);
+
+/*  use of String Aaddition */
+
+let g = "Nusaibah";
+let h = "Shanza";
+console.log("String result:", g + " " + h);
+
+/*  logical Oparetor && ! || */
+
+/* uses of if and else */
+
+let l = 40;
+let k = 70;
+
+/* if (l == 50) {
+  console.log("show me");
+} else {
+  console.log("if its falase show me :");
+} */
+
+if (l < 25) {
+  console.log("logic is true");
+} else {
+  console.log("ligic is false");
+}
+
+console.log(l <= 40);
+console.log(k >= 100);
